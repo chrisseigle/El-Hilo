64fe8038e06fa0456987276751c99623ff4e8dc6
Chris Seigle <68213471+chrisseigle@users.noreply.github.com>
2024-04-24 19:09:51 -0600

    pag

---
+++ enviar.php
@@ -0,0 +1,23 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $nombre = $_POST["nombre"];
+    $email = $_POST["email"];
+    
+    $destinatario = "mktseigle@gmiale.com"; // Cambia esto por tu dirección de correo electrónico
+    $asunto = "Nuevo mensaje de contacto";
+
+    $mensaje = "Nombre: $nombre\n";
+    $mensaje .= "Email: $email\n";
+
+    // Si quieres añadir más campos al mensaje, agrégalos aquí
+
+    // Envía el correo electrónico
+    mail($destinatario, $asunto, $mensaje);
+
+    // Redirecciona a una página de confirmación o muestra un mensaje de éxito
+    header("Location: merceria.html");
+} else {
+    // Si el formulario no se envió por el método POST, redirige a la página del formulario
+    header("Location: nosotros.html");
+}
+?>
